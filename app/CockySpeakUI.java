db032c519b7aace7fef8b00ed16aa4a331f0ae93
Bryce Klein <bryceian2003@gmail.com>
2024-10-01 08:52:29 -0400

    basic driver to get UserID for json first submission

---
+++ app/CockySpeakUI.java
@@ -0,0 +1,12 @@
+package app;
+
+import java.util.UUID;
+
+public class CockySpeakUI {
+
+    public static void main(String[] args) {
+        String uniqueID = UUID.randomUUID().toString();
+        System.out.println(uniqueID);
+    }
+
+}
