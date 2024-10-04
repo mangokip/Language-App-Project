62a9969a7a7cb662b1aa75f6083ba80307569b8c
dd010504 <@dd26@email.sc.edu>
2024-10-03 21:18:56 -0400

    Data Loader Stub

---
+++ speak/src/main/DataLoader.java
@@ -0,0 +1,37 @@
+/**
+ * Data loader
+ * @author By David Dinh
+ */
+package com.narration.data;
+
+//need to determine where files are going to be 
+//import com.narration.User;
+//import com.narration.Language;
+//import com.narration.Lesson;
+//import java.util.List;
+
+public class DataLoader {
+
+    // Constructor
+    public DataLoader() {
+        // Initialization, if needed
+    }
+
+    // Method to load the list of users from a JSON file
+    public List<User> getUserList() {
+        // Stub: To be implemented
+        return null;
+    }
+
+    // Method to load the list of languages from a JSON file
+    public List<Language> getLanguageList() {
+        // Stub: To be implemented
+        return null;
+    }
+
+    // Method to load lessons from a JSON file
+    public List<Lesson> getLessonList() {
+        // Stub: To be implemented
+        return null;
+    }
+}
