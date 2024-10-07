e86ef904f8c16c122d717eb69094ab4f27c63b70
dd010504 <@dd26@email.sc.edu>
2024-10-06 22:13:13 -0400

    #2

---
+++ app/DataLoader.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+/**
+ * The DataLoader class is responsible for loading data related to users and languages
+ * into the CockySpeak application. It provides methods to retrieve user and language lists.
+ * @author David Dinh
+ */
+public class DataLoader {
+
+    /**
+     * Loads and returns the list of users from the data source.
+     * 
+     * @return an ArrayList of User objects
+     */
+    public ArrayList<User> getUserList() {
+        // TODO: Add logic to load users from the data source (e.g., file, database)
+        return null; // placeholder
+    }
+
+    /**
+     * Loads and returns the list of languages from the data source.
+     * 
+     * @return an ArrayList of Language objects
+     */
+    public ArrayList<Language> getLanguageList() {
+        // TODO: Add logic to load languages from the data source (e.g., file, database)
+        return null; // placeholder
+    }
+}
