979754c30c97480c4b3dd776635d0d27c7da4424
mangokip <biklein@email.sc.edu>
2024-10-06 20:25:25 -0400

    updated javadoc for User.java

--- app/User.java
+++ app/User.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
  * for those who will be using the CockySpeak application.
  */
 public class User {
+
     private String username;
     private String password;
     private String email;
@@ -18,54 +19,81 @@ public class User {
 
     }
 
-    /*
-     * Parametized Constructor
+    /**
+     * Parametized Constructor - ASK IF WE NEED TO ADD UUID TOO
+     *
+     * @param username - user's username of choice
+     * @param password - user's password of choice
+     * @param email - user's registered email
      */
     public User(String username, String password, String email) {
 
     }
 
-    /*
-     * This method will register a user with only a username and password.
-     */
-    public void register(String username, String password) {
-
-    }
-
-    /*
-     * Overloaded method for if the user wants to register an email as well
+    /**
+     * Method to register user given attributes
+     *
+     * @param username - user's username to be passed
+     * @param password - user's password to be passed
+     * @param email - user's email to be passed
      */
     public void register(String username, String password, String email) {
 
     }
 
-   /*
-    * 
-    */
-    public boolean isMatch(String username, String password) {
+    /**
+     * Independent check of match for email to be integrated with resetPassword
+     *
+     * @param email - email to be checked
+     * @return - return boolean, true if match false if not
+     */
+    public boolean isMatch(String email) {
 
     }
 
-    /*
-     * Resets password based on user's email.
+    /**
+     * Basic check on if user passed is a match to an existing user
+     *
+     * @param username - username to be checked
+     * @param password - password to be checked
+     * @param email - email to be checked
+     * @return - return boolean, true if match false if not
+     */
+    public boolean isMatch(String username, String password, String email) {
+
+    }
+
+    /**
+     * Resets user's password based on passed email if it exists with a user
+     *
+     * @param email
      */
     public void resetPassword(String email) {
 
     }
 
-    /*
-     * Get method for users
-     * @return User object
+    /**
+     * Grabs user object
+     *
+     * @return - User object
      */
     public User getUser() {
 
     }
 
+    /**
+     * User's language of choice to be selected
+     *
+     * @param language - language to be chosen
+     */
     public void selectedLanguage(Language language) {
 
     }
 
-   
+    /**
+     * Grabs a ProgressTracker object to be linked to a user for each language they are learning
+     * @return - HashMap<Language (key of what the progressTracker will be assigned to), ProgressTracker (progress tracker object to be linked to language)>
+     */
     public HashMap<Language, ProgressTracker> getProgressTracker() {
 
     }
