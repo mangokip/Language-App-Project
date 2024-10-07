5186446504cc59c22481bec137ff500ab6d35538
mangokip <biklein@email.sc.edu>
2024-10-06 22:14:48 -0400

    #6 UI stub finished for most part - added basic main, will revisit as we progress in class with GUI

--- app/CockySpeakUI.java
+++ app/CockySpeakUI.java
@@ -7,6 +7,7 @@ public class CockySpeakUI {
     public static void main(String[] args) {
         String uniqueID = UUID.randomUUID().toString();
         System.out.println(uniqueID);
+        //todo - add stub info in future
     }
 
 }
