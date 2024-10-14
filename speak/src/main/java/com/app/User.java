7fc41674850b6b8eec4ea7aa5432c3d53623a672
mangokip <biklein@email.sc.edu>
2024-10-14 11:53:21 -0400

    removed redundant methods

--- speak/src/main/java/com/app/User.java
+++ speak/src/main/java/com/app/User.java
@@ -83,18 +83,7 @@ public class User {
 
     }
 
-    public ArrayList getLanguageProgress() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getLanguageProgress'");
-    }
-    public Object getUUID() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUUID'");
-    }
-    public Object getUsername() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
-    }
+   
 
 }
 
