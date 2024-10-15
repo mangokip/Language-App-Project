033ffbe5dc916efedad1394187298a8abf9752e6
Fred Pearson IV <Fredpearsoniv@gmail.com>
2024-10-15 09:50:15 -0400

    adding the word class

--- speak/src/main/java/com/app/Word.java
+++ speak/src/main/java/com/app/Word.java
@@ -4,14 +4,20 @@ package com.app;
  */
 public class Word {
     private String text;
+    private String foreign;
+    private String pronounce;
+    private Genre genre;
 
     /**
      * Constructs a Word object with the specified text.
      * 
      * @param text the word as a String
      */
-    public Word(String text) {
+    public Word(String text, String foregin, String pronounce, Genre genre) {
         this.text = text;
+        this.foreign = foreign;
+        this.pronounce = pronounce;
+        this.genre = genre;
     }
 
     /**
@@ -23,6 +29,20 @@ public class Word {
         return text;
     }
 
+    public String getForeign()
+    {
+        return foreign;
+    }
+
+    public String getPronounce(){
+        return pronounce;
+    }
+
+    public Genre getGenre()
+    {
+        return genre;
+    }
+
     /**
      * Sets the word text.
      * 
@@ -32,13 +52,42 @@ public class Word {
         this.text = text;
     }
 
-    /**
-     * Analyzes the word to extract linguistic information such as part of speech.
-     * 
-     * TODO: Implement the logic for word analysis.
-     */
-    public void analyze() {
-        // TODO: Implement logic to analyze the word (e.g., part of speech, meaning)
-        System.out.println("Analyzing word: " + text);
+    public void setForeign(String foreign){
+        this.foreign = foreign;
+    }
+
+    public void setPronounce(String pronounce){
+        this.pronounce = pronounce;
+    }
+
+    public  void setGenre(Genre genre){
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+               "text='" + text + '\'' +
+               ", foreign='" + foreign + '\'' +
+               ", pronounce='" + pronounce + '\'' +
+               ", genre=" + genre +
+               '}';
     }
 }
+
+enum Genre {
+    NOUN, VERB, ADJECTIVE, ADVERB, PREPOSITION, CONJUNCTION, INTERJECTION
+
+
+    /**
+    //  * Analyzes the word to extract linguistic information such as part of speech.
+    //  * 
+    //  * TODO: Implement the logic for word analysis.
+    //  */
+    // public void analyze() {
+    //     // TODO: Implement logic to analyze the word (e.g., part of speech, meaning)
+    //     System.out.println("Analyzing word: " + text);
+    // }
+
+}
+
