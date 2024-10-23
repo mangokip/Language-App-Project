d526e34c54eb4dd13ed8fbcdb9fb8b9b28c3be91
mangokip <biklein@email.sc.edu>
2024-10-23 13:00:34 -0400

    final team additions made

--- speak/src/main/java/com/app/CockySpeakUI.java
+++ speak/src/main/java/com/app/CockySpeakUI.java
@@ -1,9 +1,11 @@
 package com.app;
+
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class CockySpeakUI {
+
     private static CockySpeak cockySpeak = new CockySpeak(); // Initialize CockySpeak
-    
 
     public static void main(String[] args) {
         scenario2();
@@ -25,11 +27,24 @@ public class CockySpeakUI {
         String email = keyboard.nextLine();
         cockySpeak.register(username, password, email);
         cockySpeak.login(username, password);
-        
+
         Language selectedLanguage = cockySpeak.promptLanguageSelection();
         cockySpeak.setLanguage(selectedLanguage);
         cockySpeak.promptDifficultySelection(selectedLanguage);
-    
+
+        WordList wordList = WordList.getInstance();
+        Word correctWord = wordList.getRandomWord(selectedLanguage);
+        ArrayList<Word> wordsForPhrase = new ArrayList<Word>();
+        wordsForPhrase.add(correctWord);
+        for (int i = 0; i < 3; i++) {
+            wordsForPhrase.add(wordList.getRandomWord(selectedLanguage));
+        }
+        Phrase phrase = new Phrase(wordsForPhrase, "nothing");
+        cockySpeak.playFillBlank(1, correctWord, phrase, selectedLanguage);
+
         //todo - rest of scenario
     }
 }
+
+
+    
